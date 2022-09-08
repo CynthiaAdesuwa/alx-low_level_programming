05b533ec82a8dd770826563b51f42dbff13299ca
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-08 07:13:39 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-}	
+{
+		char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(Mystr);
+
+				return (0);
+}
