f2f153a8fd82078610afacd8ed936c57af10cc7a
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-08 15:28:58 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
- * main - This program prints a sentence and a date
- * followed by a new line, to the standard error.
+ *
+ *  * main - This program prints a sentence and a date
  *
  * Return: 1 for success
  */
-
 int main(void)
-
-{ write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
