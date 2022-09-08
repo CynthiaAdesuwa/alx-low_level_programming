bde52a216845c8aaa0321a20d3e7e91f05bdd750
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-08 09:38:37 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  *main - print the string in the put function
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
-		char Mystr[] = "with proper grammar, but the outcome is a piece of art,\n";
-		        printf(Mystr);
-			        return (0); 
-} 
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+ 
