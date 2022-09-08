61ea86cb494f22c24e9a096c9b648759fcba7f74
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-08 07:18:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+* main - Entry point
+*
+* Return: Always 0 (success)
+ */
 int main(void)
 {
 		char Mystr[] = "\"Programming is like building a multilingual puzzle";
