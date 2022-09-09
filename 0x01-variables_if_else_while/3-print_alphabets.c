41cc7ab11b49f58d9dd42e5c9d615ee5cef47d09
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-09 16:07:04 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'A' ; ch <= 'z' ; ch++)
+putchar(ch);
+		putchar('\n');
+return (0);
+}
