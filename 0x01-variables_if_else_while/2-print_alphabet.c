fbb6bb22fbb0b2ad9205c5162c8bb3910bc25821
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-09 15:27:39 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,9 +5,9 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{										char ch;
+{												char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
-		putchar(ch);
+			putchar(ch);
 putchar('\n'); 
 return (0);
 }
