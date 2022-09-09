19735a5bb09110aa506e6121af5bd6126a916b33
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-09 16:57:36 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int d;
+for (d = '0' ; d < 10 ; d++)
+printf("%d", d);
+		printf("\n");
+return (0);
+}
