0d1c7a4bcf168c2468cb2ae7a0e9e413ca7fea4c
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-09 19:53:46 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,31 +6,14 @@
  */
 int main(void)
 {
-
-		int c;
-
-
-
-			for (c = 48; c <= 57; c++)
-
-					{
-
-								putchar(c);
-
-										if (c != 57)
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{										putchar(',');
+putchar(' ');									}
+}
 				putchar('\n');
-
-					return (0);
-
+return (0);
 }
