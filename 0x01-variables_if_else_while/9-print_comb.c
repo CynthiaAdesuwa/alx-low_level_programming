450de863bfb3031c8a7ed2a18215bdad41e9a6f2
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-09 18:21:20 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - printing
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int d;
-for (d = 48; d <= 57; d++)
-putchar(d);
-if (d = 57)
+int c;
+for (c = 48; c <= 57; c++)
+putchar(c);
+if (c != 57)
 putchar(',');
 putchar(' ');
 		putchar('\n');
