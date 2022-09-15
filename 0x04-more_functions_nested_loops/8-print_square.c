b60348a852c19a943f893da9c017c752119500e1
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-15 14:57:48 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,39 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
- * print_square - prints squares
- * @size: parameter
- * Return: returns nothing
+ * main - prints the numbers from 1 to 100, followed by a new line 
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
  */
-void print_square(int size)
+int main(void)
 {
-	int inc1, inc2;
-
-	if (size > 0)
-	{
-		for (inc1 = 0; inc1 < size; inc1++)
+	int i; 
+	for (i = 1; i <= 100; i++) 
+	{ 
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-			{
-				putchar('#');
-			}
-			putchar('#');
-			putchar('\n');
+			printf(" Fizz");
 		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{ 
+			printf(" Buzz");
+		}
+		 else if (i % 3 == 0 && i % 5 == 0)
+		 {
+			 printf(" FizzBuzz");
+		 }
+		  else if (i == 1) 
+		  { 
+			  printf("%d", i);
+		  }
+		   else 
+		   {
+			   printf(" %d", i);
+		   }
 	}
-	else
-	{
-		putchar('\n');
-	}
+	printf("\n"); 
+	 
+
+	return (0); 
 }
