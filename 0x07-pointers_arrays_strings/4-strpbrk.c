a6074c5bb01a73369cef17ecddf3a7a73d5866bd
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-26 15:59:39 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,8 +13,8 @@ char *_strpbrk(char *s, char *accept)
 	for (i = 0; *(s + i); i++)
 	{
 		for
-			(j = 0;)
-			*(accept + j); (j++);
+			(j = 0;
+			*(accept + j); (j++));
 		{
 			if (*(s + i) == *(accept + j))
 			{
