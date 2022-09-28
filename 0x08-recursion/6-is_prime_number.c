f25d459f86f09bafa3a37f5492826a3a79e1d3fb
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-28 09:35:57 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,13 +2,26 @@
 /**
  * is_prime_number -  returns the 1 if n is prime
  * @n: The number to be checked
- * Return: If the number is divisible - 0
- * If the number is not divisible - 1
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
 	int start = n / 2;
 
 	if (n <= 1)
+		return (0);
+}
+
+/**
+ * is_prime - returns the 1 if n is prime
+ * @n: The number to be checked
+ * @start: number to start checking from
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	if (n % start == 0)
 		return (0);
 }
