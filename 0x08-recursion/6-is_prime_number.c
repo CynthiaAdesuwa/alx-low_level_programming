1861bd666d73e2368453421a1f55314847d3a7c8
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-09-28 08:43:14 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number -  returns the 1 if n is prime
+ * @n: The number to be checked
+ * Return: If the number is divisible - 0
+ * If the number is not divisible - 1
+ */
+int is_prime_number(int n)
+{
+	int start = n / 2;
+
+	if (n <= 1)
+		return (0);
+}
