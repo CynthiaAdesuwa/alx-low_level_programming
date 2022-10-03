b9366401bf91f4989ce02ab98b6416d522a038e8
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-03 03:37:26 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -41,9 +41,9 @@ char *argstostr(int ac, char **av)
 			j = 0;
 			while (av[i][j])
 			{
-				arg[k] = (av[i][j]; j++)
+				arg[k] = (av[i][j]), j++;
 			}
-			arg[k] = ('\n'; k++)
+			arg[k] = '\n'; k++
 			i++;
 		}
 		arg[k] = '\0';
