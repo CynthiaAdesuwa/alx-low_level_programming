ce134f459bb534edb7c49184b0cf0fd2ec14bcee
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-06 14:43:42 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 	unsigned long mul;
 	int i, j;
 
-	if (argc != 3)
+	if (argc != 0)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
