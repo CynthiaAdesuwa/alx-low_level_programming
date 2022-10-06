4b197b0d32d574de14bc8a7b47af549bcd407fe7
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-06 11:59:48 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
-char *create_array(unsigned int size, char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
