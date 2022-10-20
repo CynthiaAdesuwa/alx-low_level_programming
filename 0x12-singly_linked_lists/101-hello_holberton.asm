6972392a0cf5cbb74a5501adffde1901066d0712
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-20 07:37:23 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+SECTION .text
+extern printf
+global main
+main:
+mov esi, msg
+mov edi, fmt
+call printf
+
+mov eax, 0
+ret
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-Write a function that frees a list_t list.
+Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
