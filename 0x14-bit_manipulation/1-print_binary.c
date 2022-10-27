6cfd0860a41ef320a30552e53e425b357a6df9fa
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-10-27 07:41:13 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
+	int printbin = ((n & 1) + '0');
 
-	}
-	else
+	if (n > 1)
 	{
-		_putchar('0');
+		print_binary(n >> 1);
 	}
+	_putchar(printbin);
 }
